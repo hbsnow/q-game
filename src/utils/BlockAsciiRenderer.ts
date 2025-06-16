bb7114d2c54d5d0531ee0534f9e542235bc81261
hbsnow <takahashi.yuki@classmethod.jp>
2025-06-16 13:28:00 +0900

    Fix: BlockAsciiRendererの構文エラーを修正

--- src/utils/BlockAsciiRenderer.ts
+++ src/utils/BlockAsciiRenderer.ts
@@ -161,7 +161,6 @@ export class BlockAsciiRenderer {
         
         beforeRow += highlightPrefix + beforeAscii + ' ';
         afterRow += highlightPrefix + afterAscii + ' ';
-        }
       }
       
       beforeRow += '|';
